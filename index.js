d31f465a7f210f04695aeac287ffd1601fed68cd
shu Lin <bbqlinshu@gmail.com>
2021-11-23 22:12:13 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(arg){
   // Write your code here!
+  if (arg <= 400){
+    return 'This one is on me!';
+  } else if (arg > 2000 && arg < 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (arg >2500){
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.':'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous': return 'Thank you so much.';
+    case 'not as generous': return 'Thank you.';
+    default : return'Bye.'
+  }
 }
